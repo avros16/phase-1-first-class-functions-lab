f3635db2414cd36765057cbed182aaf2d5da0c7c
Ana Rosario <arosario.mrd@gmail.com>
2022-11-01 22:08:22 -0400

    commit tree 50c48a1e7cd6aaad23c1b6e4828da6db56f69ab0

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2)
+};
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function (num1) {
+        return num1 * fare
+
+    };
+
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier (fare)(2);
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier (fare)(3);
+}
+
+function selectDifferentDrivers(drivers,returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
